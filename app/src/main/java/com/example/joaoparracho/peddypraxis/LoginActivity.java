355d915a6307cb87d4937e9010205e1a1ab23c47
joaoparracho <jparracho@live.com.pt>
2018-12-05 14:07:24 +0000

    #2 parracho init

--- app/src/main/java/com/example/joaoparracho/peddypraxis/LoginActivity.java
+++ app/src/main/java/com/example/joaoparracho/peddypraxis/LoginActivity.java
@@ -1,11 +1,16 @@
 package com.example.joaoparracho.peddypraxis;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
@@ -22,7 +27,27 @@ public class LoginActivity extends AppCompatActivity {
         firebaseAuth= FirebaseAuth.getInstance();
     }
     public void onClickLogin(View v){
-        TextView userEmail = findViewById(R.id.TextViewEmail);
+
+        //if(validate()) {
+            TextView userEmail = findViewById(R.id.TextViewEmail);
+            TextView userPassword = findViewById(R.id.TextViewPass);
+
+            String email = userEmail.getText().toString();
+            String pass = userPassword.getText().toString();
+
+            firebaseAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()) {
+                        Toast.makeText(LoginActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(LoginActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        //}
+
     }
     public void onCLickSignUp(View v){
 
